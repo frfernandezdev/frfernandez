af0f907faab17f2a2c08be24e208c06471d05263
Fernando Fernandez <frfernandezdev@gmail.com>
2021-06-15 22:18:46 -0400

    add useOnceCall and useScrollTrigger

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,53 @@
 import { useRef, useEffect } from 'react'; 
 
-export function useOnceCall(cb, condition) {
+export function useOnceCall(cb) {
 	const isCallRef = useRef(false);
 
 	useEffect(() => {
-		if (!condition && isCallRef.current) return;
+		if (isCallRef.current) return;
 		isCallRef.current = true;
-		cb();
-	},[ cb, condition ]);
+		return cb();
+	}, [ cb ]);
+};
+
+export function useScrollTrigger(cb, delay=null) {
+	const dir = useRef(1);
+	const scrollSensitivitySettings = 30;
+	const rId = useRef(null);
+
+	useEffect(() => {
+		const isFirefox  = /Firefox/i.test(navigator.userAgent);
+		const mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+
+		function handler(__dir) {
+			dir.current = __dir;
+			return () => {
+				rId.current = null;
+				cb(dir.current);
+			}
+		}
+
+		function listener(event) {
+			let delta = isFirefox ? event.detail * (-120) : event.wheelDelta; 
+			let __dir = delta <= -scrollSensitivitySettings ? 0 :
+									delta >= scrollSensitivitySettings ? 1 : 0;
+
+			if (!delay) {
+				if (dir.current === __dir) return;
+
+				dir.current = __dir;
+				cb(dir.current);
+				return;
+			};
+
+			if (rId.current) return;
+			rId.current = setTimeout(handler(__dir), delay);
+		}
+
+		window.addEventListener(mousewheelEvent, listener)	
+		return () => {
+			window.removeEventListener(mousewheelEvent, listener)
+		};
+	}, [ cb, delay ]);
+
 };
