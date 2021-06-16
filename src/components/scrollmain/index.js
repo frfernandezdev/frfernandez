1875bbb3e609efbff8a0c218327d600f7c3fd8e0
Fernando Fernandez <frfernandezdev@gmail.com>
2021-06-15 22:21:30 -0400

    add scrollmain

---
+++ src/components/scrollmain/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as styles from './index.module.css'; 
+
+
+const ScrollMain = ({children, value}) => {
+	return (
+		<main>
+			{children}
+		</main>
+	);
+}
+
+export default ScrollMain;
