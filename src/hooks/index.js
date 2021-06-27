4ba7d0f6f61425ce495f49072ed88089a4a8fe6a
Fernando Fernandez <frfernandezdev@gmail.com>
2021-06-26 22:07:43 -0400

    add touch scroll support for mobile

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useRef, useEffect } from 'react'; 
+import { useRef, useEffect, useCallback } from 'react'; 
+import { isMobile, isBrowser } from '../utils';
 
 export function useOnceCall(cb) {
 	const isCallRef = useRef(0);
@@ -14,40 +15,63 @@ export function useScrollTrigger(cb, delay=null) {
 	const dir = useRef(1);
 	const scrollSensitivitySettings = 30;
 	const rId = useRef(null);
+	const move = useRef(0);
 
-	useEffect(() => {
-		const isFirefox  = /Firefox/i.test(navigator.userAgent);
-		const mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+	const handler = useCallback(function(direction) {
+		if (direction === null) return 0;
+		if (!delay) {
+			if (dir.current === direction) return 0;
 
-		function handler(__dir) {
-			dir.current = __dir;
-			return () => {
-				rId.current = null;
-				return cb(dir.current);
-			}
+			dir.current = direction;
+			return cb(dir.current);
 		}
+
+		function handlerTimeout() {
+			rId.current = null;
+			return cb(dir.current)
+		}
+		if (rId.current) return 0;
+		dir.current = direction;
+		rId.current = setTimeout(handlerTimeout, delay);
+	}, [ delay, cb ]);
+
+	// Listener for mobile
+	useEffect(function() {
+		function listenerStart(event) {
+			move.current = event.touches[0].clientY;
+		}
+
+		function listenerEnd(event) {
+			let end = event.changedTouches[0].clientY;
+			return handler(move.current >= (end +scrollSensitivitySettings) ? 0 : 
+										 move.current <= (end -scrollSensitivitySettings) ? 1 : null);
+		}
+
+		window.addEventListener('touchstart', listenerStart);
+		window.addEventListener('touchend', listenerEnd);
+		return () => {
+			window.removeEventListener('touchstart', listenerStart);
+			window.removeEventListener('touchend', listenerEnd);
+		};
+	}, [ handler ]);
+
+	// Listener for browser
+	useEffect(function() {
+		let eventName = isBrowser('Firefox') ? "DOMMouseScroll" : "wheel";
 
 		function listener(event) {
-			let delta = isFirefox ? event.detail * (-120) : event.wheelDelta; 
-			let __dir = delta <= -scrollSensitivitySettings ? 0 :
-									delta >= scrollSensitivitySettings ? 1 : 0;
-
-			if (!delay) {
-				if (dir.current === __dir) return 0;
-
-				dir.current = __dir;
-				return cb(dir.current);
-			};
-
-			if (rId.current) return;
-			rId.current = setTimeout(handler(__dir), delay);
+			let delta = isBrowser('Firefox') ? 
+											event.detail * (-120) : event.wheelDelta;
+			return handler(delta <= -scrollSensitivitySettings ? 0 :
+						 				 delta >= scrollSensitivitySettings ? 1 : 0);
 		}
 
-		window.addEventListener(mousewheelEvent, listener)	
+		window.addEventListener(eventName, listener);
+		window.addEventListener(eventName, listener);
 		return () => {
-			clearTimeout(rId.current);
-			window.removeEventListener(mousewheelEvent, listener);
-		}
-	}, [ cb, delay ]);
+			window.removeEventListener(eventName, listener);
+			window.removeEventListener(eventName, listener);
+		};
+	}, [ handler ]);
 
 };
