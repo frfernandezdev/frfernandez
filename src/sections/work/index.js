c84f892d9da6e7176d9f10b0163af4f9c2402cbf
Fernando Fernandez <frfernandezdev@gmail.com>
2021-06-26 22:16:40 -0400

    update work

--- src/sections/work/index.js
+++ src/sections/work/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import './index.module.css';
+import { StaticImage } from 'gatsby-plugin-image';
+import * as styles from './index.module.css';
 
 
-export default function About() {
+export default function Work() {
 	return (
-		<section>Work</section>
+		<section id={styles.work}>
+			<nav className={styles.navTitle}>				
+				<div className={styles.title}>
+					<h3>Work</h3>
+				</div>
+				<div className={styles.container}></div>
+			</nav>
+			<div className={styles.container}>
+				<div className={styles.projects}>
+					<div className={styles.project}>
+						<div className={styles.browser}>
+							<StaticImage
+								src="../../images/conexperto.svg"
+								alt="Avatar"
+								placeholder="blurred"
+								className={styles.projectImg}	
+								objectFit="contain"
+							/>
+						</div>
+						<div className={styles.mobile + ' ' + styles.over}>
+							<StaticImage
+								src="../../images/conexperto.svg"
+								alt="Avatar"
+								placeholder="blurred"
+								className={styles.projectImg}	
+								objectFit="contain"
+							/>
+						</div>
+					</div>
+					<div className={styles.project}></div>
+					<div className={styles.project}></div>
+					<div className={styles.project}></div>
+					<div className={styles.project}></div>
+					<div className={styles.project}></div>
+					<div className={styles.project}></div>
+				</div>				
+			</div>
+		</section>
 	); 
 }
 
