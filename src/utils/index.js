3af409f2831f6e6a4aa915abc65d57549e41b7cf
Fernando Fernandez <frfernandezdev@gmail.com>
2021-06-26 22:06:51 -0400

    add isMobile and isBrowser

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,3 +12,33 @@ export function getBoundingClientRect(el, dir=null) {
 				: el.current.getBoundingClientRect();
 }
 
+export function isMobile(agent=null) {
+	const options = {
+		'Android': () => /Android/i.test(navigator.userAgent),
+		'IOS': () => /iPhone|iPad|iPod/i.test(navigator.userAgent),
+		'Opera': () => /Opera Mini/i.test(navigator.userAgent),
+		'Windows': () => /WPDesktop/i.test(navigator.userAgent)
+	}
+	return options[agent] || (
+		options['Android']() ||
+		options['IOS']() ||
+		options['Opera']() ||
+		options['Windows']()
+	)
+}
+
+export function isBrowser(agent=null) {
+	const options = {
+		'Firefox': () => /Google/i.test(navigator.vendor), 
+		'Safari': () => /Apple/i.test(navigator.vendor),
+		'Chrome': () => /Chrome/i.test(navigator.userAgent),
+		'Opera': () => /Opera/i.test(navigator.userAgent),
+		'edge': () => /Edge/i.test(navigator.userAgent),
+	}
+	return options[agent] || (
+		options['Firefox']() ||
+		options['IOS']() ||
+		options['Opera']() ||
+		options['Windows']()
+	);
+}
