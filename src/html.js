7559c85531a09b8fa0aa0da1d3b288bcb9f3d531
Fernando Fernandez <frfernandezdev@gmail.com>
2021-07-01 21:27:35 -0400

    add meta tag cannonical

--- src/html.js
+++ src/html.js
@@ -13,7 +13,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 				<meta httpEquiv="ScreenOrientation" content="autoRotate:disabled" />
-				
+				<link rel="canonical" href="https://frfernandez.com"/>				
 				<title>FrFernandez - Web Developer</title>
 				<meta name="title" content="FrFernandez - Web Developer" />
 				<meta name="description" content="My personal portfolio" />
